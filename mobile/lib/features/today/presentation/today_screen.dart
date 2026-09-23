0e90b3f62d0664427256a7991c406917ff55f64f
Sifat <105747804+sifat371@users.noreply.github.com>
2026-09-24 04:34:48 +0600

    Refresh Today while open as dose status changes

--- mobile/lib/features/today/presentation/today_screen.dart
+++ mobile/lib/features/today/presentation/today_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:familymed/core/auth/auth_controller.dart';
 import 'package:familymed/features/today/data/today_repository.dart';
 import 'package:familymed/features/today/presentation/dose_card.dart';
@@ -6,11 +8,32 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-class TodayScreen extends ConsumerWidget {
+class TodayScreen extends ConsumerStatefulWidget {
   const TodayScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<TodayScreen> createState() => _TodayScreenState();
+}
+
+class _TodayScreenState extends ConsumerState<TodayScreen> {
+  Timer? _refreshTimer;
+
+  @override
+  void initState() {
+    super.initState();
+    _refreshTimer = Timer.periodic(const Duration(minutes: 1), (_) {
+      ref.invalidate(todayProvider);
+    });
+  }
+
+  @override
+  void dispose() {
+    _refreshTimer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final l10n = AppLocalizations.of(context);
     final today = ref.watch(todayProvider);
 
