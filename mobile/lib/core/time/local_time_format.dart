275f39a21142f9f69514daa4bbb1bca75db22381
Sifat <105747804+sifat371@users.noreply.github.com>
2026-09-24 23:54:20 +0600

    feat: format snooze instants in dose timezone

--- mobile/lib/core/time/local_time_format.dart
+++ mobile/lib/core/time/local_time_format.dart
@@ -1,4 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:timezone/data/latest.dart' as tzdata;
+import 'package:timezone/timezone.dart' as tz;
+
+bool _timeZonesInitialized = false;
+
+void _ensureTimeZones() {
+  if (_timeZonesInitialized) return;
+  tzdata.initializeTimeZones();
+  _timeZonesInitialized = true;
+}
 
 TimeOfDay timeOfDayFromLocalTime(String value) {
   final parts = value.split(':');
@@ -19,6 +29,20 @@ String localTimeFromTimeOfDay(TimeOfDay value) {
 String formatLocalTime12h(BuildContext context, String value) {
   return MaterialLocalizations.of(context).formatTimeOfDay(
     timeOfDayFromLocalTime(value),
+    alwaysUse24HourFormat: false,
+  );
+}
+
+String formatInstantInTimezone12h(
+  BuildContext context,
+  DateTime value,
+  String timezoneName,
+) {
+  _ensureTimeZones();
+  final location = tz.getLocation(timezoneName);
+  final local = tz.TZDateTime.from(value.toUtc(), location);
+  return MaterialLocalizations.of(context).formatTimeOfDay(
+    TimeOfDay(hour: local.hour, minute: local.minute),
     alwaysUse24HourFormat: false,
   );
 }
